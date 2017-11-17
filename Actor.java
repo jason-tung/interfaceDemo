6b68d4170ea02df7ea92d7fae05d0845c8e90435
konstans <konstans@stuycs.org>
2017-11-17 15:08:09 -0500

    I messed with the comments. For git demo purposes.

--- Actor.java
+++ Actor.java
@@ -1,12 +1,19 @@
+//I like pie. (also PI)
+
 //This class definition tells java that you can compare actors to
+
 //other actors.   
 
 public abstract class Actor implements Comparable<Actor>{
+
     //implements Comparable<Actor> requires you make a method
+
     //   int compareTo (Actor o)
+
     //in order to be compliant with the Comparable interface
 
     //this method will compare actors by name
+
     public int compareTo(Actor other){
 	return 0;
 	//Try 0-3 and compile and run Demo3 after each one. 
